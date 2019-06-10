a6d5ed254828147159b3cf67d1a03c8f65851b18
Miguel Arias <ctdesing@gmail.com>
2019-06-10 14:26:39 -0400

    conection fixed

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
   next();
 });
 // DATABASE CONNECTION Mongoose
-mongoose.connect("mongodb://localhost:27017/dashboard", {
+mongoose.connect("mongodb://dashboard:dashboard@jancxdashboard.ddns.net:27017/dashboard?authSource=admin", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true
