72ac0cf339726911d15cb828ed5035912a0f7d4f
Miguel Arias <ctdesing@gmail.com>
2019-06-12 23:51:49 -0400

    history filters partially implemented

--- table.js
+++ table.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-firstQuery: 'SELECT *FROM cloud_session.UserInfo WHERE start_time >= NOW() - INTERVAL 10 DAY LIMIT 50;'
+lastSevenDays: 'SELECT *FROM cloud_session.UserInfo WHERE start_time >= NOW() - INTERVAL 10 DAY LIMIT 50;'
 
 };
